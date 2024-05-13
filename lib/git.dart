67a1d96e3910cfe49eb97b6367c6902410300246
Inazamam-Ul-Haq <inzaaamam@gmail.com>
2024-05-13 15:57:15 +0500

    add git repositry

---
+++ lib/git.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GitExample extends StatefulWidget {
+  const GitExample({super.key});
+
+  @override
+  State<GitExample> createState() => _GitExampleState();
+}
+
+class _GitExampleState extends State<GitExample> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
